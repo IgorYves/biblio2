420eb9748e0870e703e9a59a037decde494a0f28
Igor <goscha931@gmail.com>
2019-03-21 09:54:00 +0100

    + user DAO

--- src/biblio/dao/UtilisateurDAO.java
+++ src/biblio/dao/UtilisateurDAO.java
@@ -1,8 +1,12 @@
 package biblio.dao;
 
-import java.util.Comparator;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import biblio.business.Adherent;
 import biblio.business.Employe;
@@ -10,64 +14,129 @@ import biblio.business.EnumCategorieEmploye;
 import biblio.business.Utilisateur;
 
 public class UtilisateurDAO {
-	private HashMap<Integer, Utilisateur> utilisateurDB = new HashMap<>();
+	private Connection connection;
 	
-	public UtilisateurDAO() {
-		Utilisateur user0 = new Adherent(0, "Ivanoff", "Vasiliy", "Joe", "pass", 
-				new GregorianCalendar(1980, 05, 25).getTime(), "m",
-				"01.45.56.78.89", "06.10.20.30.40");
-		utilisateurDB.put(0, user0);
-		
-		Utilisateur user1 = new Employe(1, "Tartempion", "Pierre", "Tart", "secret",
-				new GregorianCalendar(1990, 06, 15).getTime(), "m",
-				"487-578-DD-5", EnumCategorieEmploye.RESPONSABLE);
-		utilisateurDB.put(1, user1);
-		
-		Utilisateur user2 = new Adherent(2, "Sarkozzi", "Cerdric", "Sarko", "123", 
-				new GregorianCalendar(1985, 07, 21).getTime(), "m",
-				"01.45.66.66.89", "06.15.25.35.45");
-		utilisateurDB.put(2, user2);
-		
-		Utilisateur user3 = new Employe(3, "Tartempion", "Pierre", "Tart", "secret",
-				new GregorianCalendar(1999, 02, 11).getTime(), "m",
-				"425-514-DD-1", EnumCategorieEmploye.RESPONSABLE);
-		utilisateurDB.put(3, user3);
-		
+	public UtilisateurDAO() throws IOException {
+		this(ConnectionFactory.getDbConnection());
 	}
-
-	public Utilisateur findByKey(int id) {
-		return utilisateurDB.get(id);
-	}
-
-	public int insertUtilisateur(Utilisateur user) {
-		if (user == null) return -1;
-		int max = utilisateurDB.keySet().stream()
-				.max(Comparator.comparing(Integer::valueOf)).get();
-		user.setIdUtilisateur(++max);
-		utilisateurDB.put(max, user);
-		return max;
+	public UtilisateurDAO(Connection connection) {
+		this.connection = connection;
 	}
 	
-	public void updateUtilisateur(Utilisateur user) {
-		utilisateurDB.replace(user.getIdUtilisateur(), user);
-	}
 	
-	public void deleteUtilisateur(Utilisateur user) {
-		utilisateurDB.remove(user.getIdUtilisateur());
+
+	public Utilisateur findByKey(int id) throws SQLException {
+		connection.setAutoCommit(false);
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from utilisateur "
+				+ "left join adherent on utilisateur.IDUTILISATEUR=adherent.IDUTILISATEUR "
+				+ "left join employe on utilisateur.IDUTILISATEUR = employe.IDUTILISATEUR "
+				+ "where utilisateur.IDUTILISATEUR=" + id);
+		Utilisateur user = null;
+		if (resultSet.next()) {
+			if ((resultSet.getString("CATEGORIEUTILISATEUR"))
+					.equalsIgnoreCase("ADHERENT")) {
+				user = new Adherent(id, resultSet.getString("nom"), 
+					resultSet.getString("prenom"), 
+					resultSet.getString("pwd"), 
+					resultSet.getString("pseudonyme"), 
+					new java.util.Date((resultSet.getDate("datenaissance")).getTime()),
+					resultSet.getString("sexe"),
+					resultSet.getString("telephone"));
+			}
+			else if ((resultSet.getString("CATEGORIEUTILISATEUR"))
+					.equalsIgnoreCase("EMPLOYE")) {
+				EnumCategorieEmploye categorieEmploye = EnumCategorieEmploye.BIBLIOTHECAIRE;
+				switch ((resultSet.getString("categorieemploye")).toUpperCase()) {
+					case "BIBLIOTHECAIRE" :
+						categorieEmploye = EnumCategorieEmploye.BIBLIOTHECAIRE;
+						break;
+					case "RESPONSABLE" :
+						categorieEmploye = EnumCategorieEmploye.RESPONSABLE;
+						break;
+					case "GESTIONNAIRE_DE_FONDS" :
+						categorieEmploye = EnumCategorieEmploye.GESTIONNAIRE_DE_FONDS;
+						break;
+				}
+				user = new Employe(id, resultSet.getString("nom"), 
+						resultSet.getString("prenom"), 
+						resultSet.getString("pwd"), 
+						resultSet.getString("pseudonyme"), 
+						new java.util.Date((resultSet.getDate("datenaissance")).getTime()),
+						resultSet.getString("sexe"),
+						resultSet.getString("codematricule"),
+						categorieEmploye);
+			}
+			else {
+				System.out.println(1 / 0);
+			}
+			statement.close();
+			return user;
+		}
+		connection.rollback();
+		return null;
 	}
-	public void deleteUtilisateur(int id) {
-		utilisateurDB.remove(id);
+
+	public List<Utilisateur> findAll() throws SQLException {
+		connection.setAutoCommit(false);
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from utilisateur "
+				+ "left join adherent on utilisateur.IDUTILISATEUR=adherent.IDUTILISATEUR "
+				+ "left join employe on utilisateur.IDUTILISATEUR = employe.IDUTILISATEUR");
+		ArrayList<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
+		Utilisateur user = null;
+		while (resultSet.next()) {
+			if ((resultSet.getString("CATEGORIEUTILISATEUR"))
+					.equalsIgnoreCase("ADHERENT")) {
+				user = new Adherent(resultSet.getInt("IDUTILISATEUR"), 
+					resultSet.getString("nom"), 
+					resultSet.getString("prenom"), 
+					resultSet.getString("pwd"), 
+					resultSet.getString("pseudonyme"), 
+					new java.util.Date((resultSet.getDate("datenaissance")).getTime()),
+					resultSet.getString("sexe"),
+					resultSet.getString("telephone"));
+			}
+			else if ((resultSet.getString("CATEGORIEUTILISATEUR"))
+					.equalsIgnoreCase("EMPLOYE")) {
+				EnumCategorieEmploye categorieEmploye = EnumCategorieEmploye.BIBLIOTHECAIRE;
+				switch ((resultSet.getString("categorieemploye")).toUpperCase()) {
+					case "BIBLIOTHECAIRE" :
+						categorieEmploye = EnumCategorieEmploye.BIBLIOTHECAIRE;
+						break;
+					case "RESPONSABLE" :
+						categorieEmploye = EnumCategorieEmploye.RESPONSABLE;
+						break;
+					case "GESTIONNAIRE_DE_FONDS" :
+						categorieEmploye = EnumCategorieEmploye.GESTIONNAIRE_DE_FONDS;
+						break;
+				}
+				user = new Employe(resultSet.getInt("IDUTILISATEUR"), 
+						resultSet.getString("nom"), 
+						resultSet.getString("prenom"), 
+						resultSet.getString("pwd"), 
+						resultSet.getString("pseudonyme"), 
+						new java.util.Date((resultSet.getDate("datenaissance")).getTime()),
+						resultSet.getString("sexe"),
+						resultSet.getString("codematricule"),
+						categorieEmploye);
+			}
+			else {
+				System.out.println(1 / 0);
+			}
+			utilisateurs.add(user);
+		}
+		statement.close();
+		return utilisateurs;
 	}
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, SQLException {
 		UtilisateurDAO userDAO = new UtilisateurDAO();
-		System.out.println(userDAO.findByKey(0));
-		Utilisateur user4 = new Employe(4, "Vasechkin", "Ivan", "Vasya", "5555",
-				new GregorianCalendar(2001, 01, 19).getTime(), "m",
-				"722-617-DD-8", EnumCategorieEmploye.GESTIONNAIRE_DE_FONDS);
-		int idNewUser = userDAO.insertUtilisateur(user4);
-		System.out.println("(id new user : " + idNewUser + ") " + userDAO.findByKey(idNewUser));
+		System.out.println(userDAO.findByKey(1));
+		System.out.println(userDAO.findByKey(2));
+		System.out.println("----------------");
+		System.out.println(userDAO.findAll());
 		
 	}
 
